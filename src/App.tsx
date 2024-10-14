03d96aaf505190d8c6309bc3cb65c7896d840353
Johan Hibender <jhibender@gmail.com>
2024-10-14 23:57:08 +0200

    Add header component and change logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from './result.svg';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
+        <a href="index.html">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hibender
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
         </a>
+
       </header>
     </div>
   );
